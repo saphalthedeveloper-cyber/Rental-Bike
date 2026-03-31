ad20936dbeb6d773aafc247d7f0c090122d2c8c4
saphalthedeveloper-cyber <saphalthedeveloper@gmail.com>
2026-03-31 09:31:23 +0545

    Enhance user schema with name and validation

--- models/user.js
+++ models/user.js
@@ -2,7 +2,18 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const userSchema=new Schema({
-    email:String,
-    password:String,
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
 },{timestamps:true})
 module.exports=mongoose.model('User',userSchema);
